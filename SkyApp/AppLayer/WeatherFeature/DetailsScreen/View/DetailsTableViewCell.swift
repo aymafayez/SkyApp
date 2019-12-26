5811e741d9166119c68c0ba8268c511862cb5b62
Ayman Fayez <aymanfayez277@gmail.com>
2019-12-26 23:50:38 +0200

    Details view is added

---
+++ SkyApp/AppLayer/WeatherFeature/DetailsScreen/View/DetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsTableViewCell.swift
+//  SkyApp
+//
+//  Created by Guest2 on 12/26/19.
+//  Copyright © 2019 Sky. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SkyApp/AppLayer/WeatherFeature/DetailsScreen/View/DetailsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailsViewController.swift
+//  SkyApp
+//
+//  Created by Guest2 on 12/26/19.
+//  Copyright © 2019 Sky. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
